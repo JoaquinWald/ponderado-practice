69b6780ce507d3c5f21444f208254870854b06cf
Joaquin Grunwald <joaquingrunwald@gmail.com>
2022-04-08 23:29:46 -0300

    modificado archivo js

--- ponderado-pract.js
+++ ponderado-pract.js
@@ -16,29 +16,31 @@ const notes = [
   },
 ];
 
+//Multiplicación de las notas con los créditos
 const notesWithCredit = notes.map(
   function(noteObject){
     return noteObject.note * noteObject.credit;
   }
 );
-
+//Suma de los resultados de la multiplicación
 const sumOfNotesWithCredit = notesWithCredit.reduce(
   function(valorAcumulado = 0, nuevoValor){
     return valorAcumulado + nuevoValor;
   }
 );
 
+//Obtención del valor de los créditos otorgados por materia
 const credits = notes.map(
   function(creditObject){
     return creditObject.credit;
   }
 );
-
+//Suma de los créditos otorgados por materia
 const sumOfCredits = credits.reduce(
   function(valorActual = 0, newVal){
     return valorActual + newVal;
   }
 );
 
-
+//División del resultado obtenido de la suma de las notas multiplicadas por los créditos, entre la suma de solamente los créditos
 const promedioPonderadoNotasConCreditos = sumOfNotesWithCredit / sumOfCredits;
